27455ab81d2f03bf0c5e992da2e6e30548d61de2
Keshav1712 <141055788+Keshav1712@users.noreply.github.com>
2025-12-11 01:35:58 +0530

    Update  for best case BubbleSort.cpp
    add a bool swapped variable if no swapping has been done then print already sorted and break it ;

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -3,17 +3,23 @@ using namespace std;
 
 // WAP to implememt a bubble sort using 2 loops
 int main (){
-	
-	int arr[5]={4,7,3,9,6};
+	bool swapped=0;
+	int arr[5]={1,2,3,4,5};
 	int n =sizeof(arr)/sizeof(arr[0]);
 	
 	for (int i=n-2;i>=0;i--){
 		for(int j =0;j<=i;j++){
 			if(arr[j]>arr[j+1]){
 				swap(arr[j],arr[j+1]);
+				swapped=1;
 			}
 		}
-	}
+	
+	if(swapped==0){
+		cout<<"already sorted:"<<endl;
+		break;
+	}}
+	
 	for(int i =0;i<n;i++){
 		cout<<arr[i]<<endl;
 	}
